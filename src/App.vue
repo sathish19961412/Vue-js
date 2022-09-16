3c61d5af74a667be663c99e779a1b57d4261c7b3
unknown <sathishfreelanc5@gmail.com>
2022-09-16 18:07:45 +0530

    Vuejs Structure Design

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<section>
+  <div class="container">
+      <div class="row">
+          <div class="col-md-1"></div>
+          <div class="col-md-10">
+            <div class="employee1">
+                <h1>Employees</h1>
+            </div>
+            <div class="employee1">
+              <employee-table-vue />
+            </div>
+          </div>
+          <div class="col-md-1"></div>
+      </div>
+  </div>
+</section>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import EmployeeTableVue from './components/EmployeeTable.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+     EmployeeTableVue
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.employee1
+{
+  display:flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,33 @@
+<template>
+    <table class="table">
+        <thead class="bg-info text-center">
+            <tr class="text-white">
+                <td>Employee Name</td>
+                <td>Employee Email</td>
+            </tr>
+        </thead>
+        <tbody  class="bg-secondary text-white text-center">
+            <tr>
+                <td>Sathish</td>
+                <td>sathish@gmail.com</td>
+            </tr>
+            <tr>
+                <td>Guruprasath</td>
+                <td>guruprasath@gmail.com</td>
+            </tr>
+            <tr>
+                <td>Mathew</td>
+                <td>mathew@gmail.com</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+<script>
+
+export default {
+    name :'employee-table'
+}
+</script>
+<style scoped>
+
+</style>
